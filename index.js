cf8de5ef31044d4a90fdd759003381306632caad
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-13 18:18:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ BoardMember.prototype.releasePressStatement = function() {
 
 // - `sayHi` — returns `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
 BoardMember.prototype.sayHi = function() {
-  return `"Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
