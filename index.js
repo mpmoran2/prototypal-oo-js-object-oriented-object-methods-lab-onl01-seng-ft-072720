6fb7c5b640fa9133198631c1333891c06d7c88c5
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-13 18:18:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+// In `index.js`, write a constructor function called `BoardMember` that sets the following properties: `name`, `homeState`, and `training`.
+function BoardMember (name, homeState, training) {
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+}
+// Add following methods to the class:
+// - `veto` — returns `No, I must disagree`
+BoardMember.prototype.veto = function() {
+  return `No, I must disagree`
+}
+
+// - `approve` — returns `You can do that!`
+BoardMember.prototype.approve = function() {
+  return `You can do that!`
+}
+
+// - `doCharity` — returns `I like to help people.`
+BoardMember.prototype.doCharity = function() {
+  return `I like to help people.`
+}
+
+// - `releasePressStatement` — returns `You will see great things from Scuber.`
+BoardMember.prototype.releasePressStatement = function() {
+  return `You will see great things from Scuber.`
+}
+
+// - `sayHi` — returns `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
+BoardMember.prototype.sayHi = function() {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
