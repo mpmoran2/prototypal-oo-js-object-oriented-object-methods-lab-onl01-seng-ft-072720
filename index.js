a93f23fdf00eb23a27978a501a89c0999661c91b
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-13 18:13:04 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+// In `index.js`, write a constructor function called `BoardMember` that sets the following properties: `name`, `homeState`, and `training`.
+
+// Add following methods to the class:
+// - `veto` — returns `No, I must disagree`
+
+// - `approve` — returns `You can do that!`
+
+// - `doCharity` — returns `I like to help people.`
+
+// - `releasePressStatement` — returns `You will see great things from Scuber.`
+
+// - `sayHi` — returns `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
