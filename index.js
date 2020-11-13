75ffacd975cb38ff53a2b08113d8127551d3ef77
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-13 18:14:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // In `index.js`, write a constructor function called `BoardMember` that sets the following properties: `name`, `homeState`, and `training`.
-
+function BoardMember (name, homeState, training) {
+  this.name
+  this.homeState
+  this.training
+}
 // Add following methods to the class:
 // - `veto` — returns `No, I must disagree`
 
